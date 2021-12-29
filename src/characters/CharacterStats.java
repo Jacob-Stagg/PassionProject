4eca94a92179e940053938d36789b6573a7e3ccc
Stagg Jacob <staggjacob@zipcodes-MacBook-Pro-2.local>
2021-12-29 13:45:32 -0500

    Added skeleton for character and monster stats.

--- src/characters/CharacterStats.java
+++ src/characters/CharacterStats.java
@@ -1,4 +1,57 @@
 package characters;
 
 public class CharacterStats {
+    private Integer str;
+    private Integer def;
+    private Integer mana;
+    private Integer agl;
+    private Integer hp;
+
+    public CharacterStats(Integer str, Integer def, Integer mana, Integer agl, Integer hp) {
+        this.str = str;
+        this.def = def;
+        this.mana = mana;
+        this.agl = agl;
+        this.hp = hp;
+    }
+
+    public Integer getStr() {
+        return str;
+    }
+
+    public void setStr(Integer str) {
+        this.str = str;
+    }
+
+    public Integer getDef() {
+        return def;
+    }
+
+    public void setDef(Integer def) {
+        this.def = def;
+    }
+
+    public Integer getMana() {
+        return mana;
+    }
+
+    public void setMana(Integer mana) {
+        this.mana = mana;
+    }
+
+    public Integer getAgl() {
+        return agl;
+    }
+
+    public void setAgl(Integer agl) {
+        this.agl = agl;
+    }
+
+    public Integer getHp() {
+        return hp;
+    }
+
+    public void setHp(Integer hp) {
+        this.hp = hp;
+    }
 }
--- src/monsters/MonsterStats.java
+++ src/monsters/MonsterStats.java
@@ -1,4 +1,47 @@
 package monsters;
 
 public class MonsterStats {
+    private Integer str;
+    private Integer def;
+    private Integer agl;
+    private Integer hp;
+
+    public MonsterStats(Integer str, Integer def, Integer agl, Integer hp) {
+        this.str = str;
+        this.def = def;
+        this.agl = agl;
+        this.hp = hp;
+    }
+
+    public Integer getStr() {
+        return str;
+    }
+
+    public void setStr(Integer str) {
+        this.str = str;
+    }
+
+    public Integer getDef() {
+        return def;
+    }
+
+    public void setDef(Integer def) {
+        this.def = def;
+    }
+
+    public Integer getAgl() {
+        return agl;
+    }
+
+    public void setAgl(Integer agl) {
+        this.agl = agl;
+    }
+
+    public Integer getHp() {
+        return hp;
+    }
+
+    public void setHp(Integer hp) {
+        this.hp = hp;
+    }
 }
